514c6cf4fd3b1950083a6cd0c48b8a9b58246d03
Joe Fowler <jbfowler2@crimson.ua.edu>
2015-12-13 22:02:51 -0600

    First answers from medium 1 exercises

--- lesson_3/medium_1.rb
+++ lesson_3/medium_1.rb
@@ -1,9 +1,37 @@
-# Answers to exercises from medium 1 in Lesson 3
+# Answers to Medium 1 exercises
 
 def prompt(msg)
-	puts "=> #{msg}"
+  puts "=> #{msg}"
+  gets
 end
 
 def question(num)
-	prompt("The answer to question #{num} is:")
+  prompt("Answer to question #{num}:")
 end
+
+def question1
+  question(1)
+  space = ""
+  10.times do
+    puts "#{space}The Flintstones Rock!"
+    space += " "
+  end
+  # 10.times do |number|
+  #   puts ("word" * number) + "The Flintstones Rock!"
+  # end
+end
+
+def question2
+  question(2)
+  statement = "The Flintstones Rock"
+  result = {}
+  alphabet = ("A".."Z").to_a + ("a".."z").to_a
+  alphabet.each do |letter|
+    letter_frequency = statement.scan(letter).count
+    result[letter] = letter_frequency if letter_frequency > 0
+  end
+  puts result
+end
+
+question1
+question2
