39f09309f3ce42c9700fed90f3cd0431a295b740
Joe Fowler <jbfowler2@crimson.ua.edu>
2015-12-16 15:51:37 -0600

    Added play again functionality to tic tac toe

--- lesson_4/tic_tac_toe.rb
+++ lesson_4/tic_tac_toe.rb
@@ -27,6 +27,7 @@ end
 
 def display_board(pos)
   system "clear"
+  puts "Player uses #{PLAYER_MARKER}, computer uses #{COMPUTER_MARKER}."
   puts ""
   puts "     |     |"
   puts "  #{pos[1]}  |  #{pos[2]}  |  #{pos[3]}"
@@ -93,20 +94,28 @@ def someone_wins?(brd)
   !!detect_winner(brd)
 end
 
-board = initialize_board
-
 loop do
+  board = initialize_board
+
+  loop do
+    display_board(board)
+    player_picks!(board)
+    break if someone_wins?(board) || board_full?(board)
+    computer_picks!(board)
+    break if someone_wins?(board) || board_full?(board)
+  end
+
   display_board(board)
-  player_picks!(board)
-  break if someone_wins?(board) || board_full?(board)
-  computer_picks!(board)
-  break if someone_wins?(board) || board_full?(board)
+
+  if someone_wins?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Play again? (Y/N)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
-display_board(board)
-
-if someone_wins?(board)
-  prompt "#{detect_winner(board)} won!"
-else
-  prompt "It's a tie!"
-end
+prompt "Goodbye!"
